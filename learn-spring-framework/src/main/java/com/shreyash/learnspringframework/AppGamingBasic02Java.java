f0398a7a0d4bb315208e36f02b72aa810104dd17
Shreyash-gupta09 <ShreyashKumar.Gupta@iiitb.ac.in>
2025-11-03 04:24:24 +0530

    How to use beans in other bean using methodcall and parameters (wiring)

--- learn-spring-framework/src/main/java/com/shreyash/learnspringframework/AppGamingBasic02Java.java
+++ learn-spring-framework/src/main/java/com/shreyash/learnspringframework/AppGamingBasic02Java.java
@@ -15,6 +15,8 @@ public class AppGamingBasic02Java {
         System.out.println(context.getBean("name"));
         System.out.println(context.getBean("age"));
         System.out.println(context.getBean("person"));
+        System.out.println(context.getBean("person2MethodCall"));
+        System.out.println(context.getBean("person3Parameters"));
         System.out.println(context.getBean("address"));
     }
 }
--- learn-spring-framework/src/main/java/com/shreyash/learnspringframework/HelloWorldConfiguration.java
+++ learn-spring-framework/src/main/java/com/shreyash/learnspringframework/HelloWorldConfiguration.java
@@ -3,8 +3,9 @@ package com.shreyash.learnspringframework;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-record Person(String name,int age){};
 record Address(String firstline,String city){};
+record Person(String name,int age,Address address) {};
+
 
 @Configuration
 public class HelloWorldConfiguration {
@@ -22,7 +23,17 @@ public class HelloWorldConfiguration {
     @Bean
     public Person person()
     {
-        return new Person("Priyanka",18);
+        return new Person("Priyanka",18,new Address("Neeladari","Electronic"));
+    }
+    @Bean
+    public Person person2MethodCall()
+    {
+        return new Person(name(),age(),address());
+    }
+    @Bean
+    public Person person3Parameters(String name,int age,Address address)
+    {
+        return new Person(name,age,address);
     }
     @Bean
     public Address address()
